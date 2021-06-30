696963407cc860773788c121ec5bc35085a53890
Max Penet <max.penet@exoscale.ch>
2021-06-30 15:33:10 +0200

    Version 1.0.0-alpha5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/auspex "1.0.0-alpha4"
+(defproject cc.qbits/auspex "1.0.0-alpha5-SNAPSHOT"
   :description "Mini wrapper over java-11 CompletableFuture with a manifold deferred after-taste"
   :url "https://github.com/mpenet/auspex"
   :deploy-repositories [["snapshots" :clojars] ["releases" :clojars]]
