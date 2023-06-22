ac2cd6338156bd583a5905f399994e82044e3b64
Max Penet <mpenet@s-exp.com>
2023-06-22 12:09:35 +0200

    fix loop/recur stack consumption (#6)
    prior to this change we would call the inner function over and over (via apply)
    instead of calling clojure.core/recur

--- src/qbits/auspex.clj
+++ src/qbits/auspex.clj
@@ -154,33 +154,36 @@
   (let [pairs (partition 2 bindings)
         vars (map first pairs)
         vals (map second pairs)
-        var-syms (map (fn [_] (gensym "var")) vars)]
+        var-syms (map (fn [_] (gensym "var")) vars)
+        ok (gensym "ok")
+        ret (gensym "ret")]
     `(let [result# (future)]
        ((fn fun# [result# ~@var-syms]
           (clojure.core/loop [~@(interleave vars var-syms)]
-            (let [ret# (try
+            (let [~ret (try
                          ~@body
                          (catch Throwable t#
                            (error! result# t#)))]
               (cond
-                (future? ret#)
-                (handle ret#
-                        (fn [ok# err#]
+                (future? ~ret)
+                (handle ~ret
+                        (fn [~ok err#]
                           (cond
                             err#
                             (error! result# err#)
 
-                            (recur? ok#)
-                            (apply fun# result# @ok#)
-
+                            (recur? ~ok)
+                            (apply fun# result# @~ok)
                             :else
-                            (success! result# ok#))))
+                            (success! result# ~ok))))
 
-                (recur? ret#)
-                (apply fun# result# @ret#)
+                (recur? ~ret)
+                (~'recur ~@(map
+                            (fn [n] `(nth @~ret ~n))
+                            (range (count vars))))
 
                 :else
-                (success! result# ret#)))))
+                (success! result# ~ret)))))
         result#
         ~@vals)
        result#)))
@@ -210,3 +213,5 @@
     @f
     (catch Exception e
       (throw (ex-unwrap e)))))
+
+
--- test/qbits/auspex_test.clj
+++ test/qbits/auspex_test.clj
@@ -256,7 +256,7 @@
                  (a/success-future 3)))))
 
 (deftest loop-recur-test
-  (is (true? @(a/loop [] true)))
+  (is (true? @(a/loop [a 1] true)))
 
   (is (= [0 1 2 3 4]
          @(a/loop [x []]
@@ -264,16 +264,16 @@
               (a/recur (conj x (count x)))
               x))))
 
-  (is (thrown? ExceptionInfo
-               @(loop [x []]
+  (is (thrown? java.util.concurrent.ExecutionException
+               @(a/loop [x []]
                   (throw ex))))
 
-  (is (thrown? ExceptionInfo
-               @(loop [x []]
+  (is (thrown? java.util.concurrent.ExecutionException
+               @(a/loop [x []]
                   (a/recur (throw ex)))))
 
-  (is (thrown? ExceptionInfo
-               @(loop [x (throw ex)])))
+  (is (thrown? ExceptionInfo ; throw up one level
+               @(a/loop [x (throw ex)])))
 
   (is (= 5
          @(a/loop [x 0]
@@ -306,6 +306,23 @@
     (is (= 1 @(a/loop [{:keys [a]} {:a 1}] a)))
     (is @(a/loop [[x & xs] [1 2 3]] (or (= x 3) (a/recur xs))))))
 
+(deftest loop-recur-stack
+  (testing "ensure loop/recur doesn't blow the stack"
+    (let [depths 10000000]
+      (is (zero? @(a/loop [i depths]
+                    (if (pos? i)
+                      (a/recur (dec i))
+                      i))))
+      (is (zero? @(a/loop [i (a/success-future depths)]
+                    (if (pos? @i)
+                      (a/recur (a/success-future (dec @i)))
+                      @i))))
+
+      (is (zero? @(a/loop [i (a/success-future depths)]
+                    (if (pos? @i)
+                      (a/recur (a/success-future (dec @i)))
+                      i)))))))
+
 (deftest let-flow-test
   (is (= 1
          @(a/let-flow [x 1]
@@ -332,7 +349,7 @@
 
 (deftest all-any-test
   (are [pred result f input] (pred result (deref (f input)))
-    =         [1 2 3]  a/all  [1 2 3]
-    =         [1 2 3]  a/all' (map future-val [1 2 3])
-    contains? #{1 2 3} a/any  [1 2 3]
+    = [1 2 3] a/all [1 2 3]
+    = [1 2 3] a/all' (map future-val [1 2 3])
+    contains? #{1 2 3} a/any [1 2 3]
     contains? #{1 2 3} a/any' (map future-val [1 2 3])))
